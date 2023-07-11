6e9c574227f831354768bc7052d780b0de5cf841
pietaMEDS <maxibanp@gmail.com>
2023-07-11 14:06:55 +0500

    bot.js

---
+++ bot.js
@@ -0,0 +1,18 @@
+const { VK } = require('vk-io');
+
+const { HearManager } = require('@vk-io/hear');
+
+const vk = new VK({
+    token: 'vk1.a.DxFkZPJCOowJlvHDltPUNkrdGR2LA-gMtiqpjJiODGGpBdGsOuYpsGprBEDVIxPQ5LesYQk46_A-mYQehA0jRFaGdYyUM733GyskR_ZRhPGOh_ag2ek8qxx4byIX0-SjQJxVahr4bXHd9z2yiZ9kDejTT4gLFyq0Qp54ay2QqayyjvcsFHV_pqLbGE7ajzzIpWieVhhrRf2yH2TuoUl7DA'
+})
+const bot = new HearManager();
+
+vk.updates.on('message_new', bot.middleware)
+
+bot.hear(/Привет/i, msg => {
+    msg.send(`Привет, я работаю`);    
+});
+
+console.log('Бот запущен!!');
+vk.updates.start().catch(console.error);
+
